f5bc32feb6a995c0f26186d11d4d1957279078c7
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-11-28 17:50:29 -0300

    Add Exercise#max_weight

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,4 +2,8 @@ class Exercise < ApplicationRecord
   has_many :workouts
 
   validates :name, uniqueness: true
+
+  def max_weight
+    workouts.map { |workout| workout.max_weight }.max
+  end
 end
--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe Exercise, type: :model do
   describe 'validations' do
     it { is_expected.to validate_uniqueness_of(:name) }
   end
+
+  describe 'methods' do
+    let!(:exercise) { create(:exercise) }
+    let!(:workout1) { create(:workout, exercise: exercise) }
+    let!(:workout2) { create(:workout, exercise: exercise) }
+    let!(:workout3) { create(:workout, exercise: exercise) }
+    let!(:repetion1) { create(:repetion, workout: workout1, reps: 10, weight: 10) }
+    let!(:repetion2) { create(:repetion, workout: workout2, reps: 10, weight: 20) }
+    let!(:repetion3) { create(:repetion, workout: workout3, reps: 10, weight: 30) }
+
+    it 'max weight' do
+      expect(exercise.max_weight).to eq(30)
+    end
+  end
 end
