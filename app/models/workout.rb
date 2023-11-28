00e211e45e91561cdd1c51b92c20ed61a5f01f67
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-11-28 17:37:41 -0300

    Add mock max_weight to Workout

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,8 @@
 class Workout < ApplicationRecord
   belongs_to :exercise
   has_many :repetions
+
+  def max_weight
+    30
+  end
 end
--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Workout, type: :model do
     it { is_expected.to belong_to(:exercise) }
     it { is_expected.to have_many(:repetions) }
   end
+
+  describe 'methods' do
+    let!(:workout) { create(:workout) }
+    let!(:repetion) { create(:repetion, workout:, reps: 10, weight: 10) }
+    let!(:repetion) { create(:repetion, workout:, reps: 10, weight: 20) }
+    let!(:repetion) { create(:repetion, workout:, reps: 10, weight: 30) }
+
+    it 'max weight' do
+      expect(workout.max_weight).to eq(30)
+    end
+  end
 end
